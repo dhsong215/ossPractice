dfdc7f8e504a85996cb97ec8851c9cee0ef4ef73
dhsong215 <dhsong215@gmail.com>
2024-03-23 23:08:30 +0900

    solve conflict

--- main.c
+++ main.c
@@ -2,15 +2,12 @@
 
 int main(void)
 {
-<<<<<<< HEAD:main.c
     int a = 1;
     int b = 5;
 
     printf("%d\n", a+b);
 
     printf("hello world\n");
-=======
     printf("woooah!\n");
->>>>>>> dhsong2/main:hello.c
     return 0;
 }
